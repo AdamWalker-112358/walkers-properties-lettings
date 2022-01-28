6ab066195cae5947b986f35d7162b58359cae554
AdamWalker-112358 <mail.adamwalker@gmail.com>
2022-01-28 22:48:56 +0200

    Refactor Navigation

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = ()=> {return(
+    <nav>
+    <ul>
+        <li> <a href="/"> Home Page </a></li>
+        <li> <a href="/about"> About Page </a></li>
+    </ul>
+  </nav>
+)}
+
+export default Navigation

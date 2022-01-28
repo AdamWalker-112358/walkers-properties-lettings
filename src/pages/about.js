6a39ab126023c9d12457ae30adf225c6d88b8b0a
AdamWalker-112358 <mail.adamwalker@gmail.com>
2022-01-28 21:59:21 +0200

    Adding About Page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Walkers Properties and Lettings</h1>
+        </main>
+    )
+}
+
+export default AboutPage;
